1e0a9d54765f72f92e7c5f431a8520d5710baddc
FrancescoXX <francescociullaroma@gmail.com>
2023-08-05 11:09:47 +0200

    edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/FrancescoXX/contentbox/edit/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/FrancescoXX/contentbox/edit/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
