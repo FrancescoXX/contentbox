d028e2725e8b7ae00209899c9ef83d12a3120ec0
FrancescoXX <francescociullaroma@gmail.com>
2023-08-07 19:12:50 +0200

    algolia url fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -72,16 +72,16 @@ const config = {
         indexName: 'contentbox',
   
         // Optional: see doc section below
-        contextualSearch: true,
+        // contextualSearch: true,
   
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
-        externalUrlRegex: 'external\\.com|domain\\.com',
+        // externalUrlRegex: 'external\\.com|domain\\.com',
   
         // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
-        replaceSearchResultPathname: {
-          from: '/docs/', // or as RegExp: /\/docs\//
-          to: '/',
-        },
+        // replaceSearchResultPathname: {
+        //   from: '/docs/', // or as RegExp: /\/docs\//
+        //   to: '/',
+        // },
   
         // Optional: Algolia search parameters
         searchParameters: {},
