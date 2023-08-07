893ffacd2d59de35084e88406668c882abf6f5be
FrancescoXX <francescociullaroma@gmail.com>
2023-08-07 22:20:42 +0200

    gtag test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,6 +37,10 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
+        gtag: {
+          trackingID: 'G-B4WTM25K0J',
+          anonymizeIP: true,
+        },
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
